712362c01fc56e309a9677745b8d94025dfb7abe
Olexandr597 <Olexandr597@gmail.com>
2023-11-09 01:37:22 +0200

    made sidebar

---
+++ js/burger_menu.js
@@ -0,0 +1,26 @@
+const iconMenu = document.querySelector('.menu_icon');
+if (iconMenu) {
+  const menuBody = document.querySelector('.menu_body');
+  const iconCross = document.querySelector('.modal_cross');
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+    // iconCross.classList.toggle('_active');
+
+
+  });
+}
+const iconCross = document.querySelector('.modal_cross');
+if (iconCross) {
+  const menuBody = document.querySelector('.menu_body');
+  iconCross.addEventListener("click", function (e) {
+    document.body.classList.remove('_lock');
+    iconMenu.classList.remove('_active');
+    menuBody.classList.remove('_active');
+    // iconCross.classList.remove('_active');
+    
+
+  });
+}
+
