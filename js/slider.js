33d62eb9f5bc9ba26f8ff2d9da5cc225ce39c444
Olexandr597 <Olexandr597@gmail.com>
2023-11-12 17:14:16 +0200

    changes slider

--- js/slider.js
+++ js/slider.js
@@ -1,75 +1,95 @@
 
-var swiper = new Swiper(".mySwiper", {
-        loop: true,
-    // loopedSlides: 3,
-    // autoplay: true,
-    // autoplayspeed: 1000,
-        // slidesPerView: 2,
-        // spaceBetween: 10,
-        freeMode: true,
-        // pagination: {
-        //   el: ".swiper-pagination",
-        //   clickable: true,
-        // },
-        breakpoints: {
-            320: {
-              slidesPerView: 1.1,
-            },
-            350: {
-                slidesPerView: 1.155,
-              },
-              370: {
-                slidesPerView: 1.2,
-              },
-              390: {
-                slidesPerView: 1.25,
-              },
-              425: {
-                slidesPerView: 1.3,
-              },
-              475: {
-                slidesPerView: 1.35,
-              },
-            500: {
-              slidesPerView: 1.5,
-            },
+// var swiper = new Swiper(".mySwiper", {
+//         loop: true,
+//     // loopedSlides: 3,
+//     // autoplay: true,
+//     // autoplayspeed: 1000,
+//         // slidesPerView: 2,
+//         // spaceBetween: 10,
+//         freeMode: true,
+//         // pagination: {
+//         //   el: ".swiper-pagination",
+//         //   clickable: true,
+//         // },
+//         breakpoints: {
+//             320: {
+//               slidesPerView: 1.1,
+//             },
+//             350: {
+//                 slidesPerView: 1.155,
+//               },
+//               370: {
+//                 slidesPerView: 1.2,
+//               },
+//               390: {
+//                 slidesPerView: 1.25,
+//               },
+//               425: {
+//                 slidesPerView: 1.3,
+//               },
+//               475: {
+//                 slidesPerView: 1.35,
+//               },
+//             500: {
+//               slidesPerView: 1.5,
+//             },
            
-        }
-      });
-      var swiper = new Swiper(".mySwiper_2", {
-        // loop: true,
-    // loopedSlides: 3,
-    // autoplay: true,
-    // autoplayspeed: 1000,
-        // slidesPerView: 2,
-        // spaceBetween: 10,
-        freeMode: true,
-        // pagination: {
-        //   el: ".swiper-pagination",
-        //   clickable: true,
-        // },
-        breakpoints: {
-            320: {
-              slidesPerView: 1.1,
-            },
-            350: {
-                slidesPerView: 1.155,
-              },
-              370: {
-                slidesPerView: 1.2,
-              },
-              390: {
-                slidesPerView: 1.25,
-              },
-              425: {
-                slidesPerView: 1.3,
-              },
-              475: {
-                slidesPerView: 1.35,
-              },
-            500: {
-              slidesPerView: 1.5,
-            },
+//         }
+//       });
+//       var swiper = new Swiper(".mySwiper_2", {
+//         // loop: true,
+//     // loopedSlides: 3,
+//     // autoplay: true,
+//     // autoplayspeed: 1000,
+//         // slidesPerView: 2,
+//         // spaceBetween: 10,
+//         freeMode: true,
+//         // pagination: {
+//         //   el: ".swiper-pagination",
+//         //   clickable: true,
+//         // },
+//         breakpoints: {
+//             320: {
+//               slidesPerView: 1.1,
+//             },
+//             350: {
+//                 slidesPerView: 1.155,
+//               },
+//               370: {
+//                 slidesPerView: 1.2,
+//               },
+//               390: {
+//                 slidesPerView: 1.25,
+//               },
+//               425: {
+//                 slidesPerView: 1.3,
+//               },
+//               475: {
+//                 slidesPerView: 1.35,
+//               },
+//             500: {
+//               slidesPerView: 1.5,
+//             },
            
-        }
-      });
+//         }
+//       });
+
+$(document).ready(function(){
+  $('.slider').slick({
+    arrows:false,
+    infinite: false,
+    centerMode:true,
+    variableWidth:true,
+  });
+});
+
+$(document).ready(function(){
+  $('.slider_sec').slick({
+    arrows:false,
+    infinite: false,
+    centerMode:true,
+    variableWidth:true,
+    // variableHight:true,
+    slidesToShow:2,
+  });
+});
