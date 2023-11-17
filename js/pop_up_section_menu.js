9105b1ed687d27e0a4572cba060bf4ad6a08ef2e
Olexandr597 <Olexandr597@gmail.com>
2023-11-17 19:02:57 +0200

    add fix code

--- js/pop_up_section_menu.js
+++ js/pop_up_section_menu.js
@@ -30,7 +30,43 @@
 //     }
 //   });
 // }
-document.addEventListener("DOMContentLoaded", function() {
+
+// for (let i = 1; i <= 9; i++) {
+//   const iconPopup = document.querySelector(`.coffee_${i}`);
+//   const menuPopup = document.querySelector(`.coffee_popup_${i}`);
+
+//   if (iconPopup && menuPopup) {
+//     const iconCrossPopup = document.querySelector('.nav_line');
+//     let startY;
+
+//     // Відкривання попапа при кліці
+//     iconPopup.addEventListener("click", function (e) {
+//       document.body.classList.toggle('_lock');
+//       iconPopup.classList.toggle('active');
+//       menuPopup.classList.toggle('active');
+//     });
+
+//     // Закриття попапа при свайпі вниз
+//     document.addEventListener('touchstart', function (event) {
+//       startY = event.touches[0].clientY;
+//     });
+
+//     document.addEventListener('touchmove', function (event) {
+//       if (!startY) return;
+
+//       const currentY = event.touches[0].clientY;
+//       const deltaY = currentY - startY;
+
+//       if (deltaY > 50 && event.target.classList.contains('nav_line')) {
+//         document.body.classList.remove('_lock');
+//         iconPopup.classList.remove('active');
+//         menuPopup.classList.remove('active');
+//         startY = null;
+//       }
+//     });
+//   }
+// }
+
 for (let i = 1; i <= 9; i++) {
   const iconPopup = document.querySelector(`.coffee_${i}`);
   const menuPopup = document.querySelector(`.coffee_popup_${i}`);
@@ -58,7 +94,14 @@ for (let i = 1; i <= 9; i++) {
       const deltaY = currentY - startY;
 
       if (deltaY > 50 && event.target.classList.contains('nav_line')) {
-        document.body.classList.remove('_lock');
+        // Додавання класу '_lock' перед затримкою
+        document.body.classList.add('_lock');
+
+        // Додавання затримки перед зняттям класу '_lock'
+        setTimeout(function () {
+          document.body.classList.remove('_lock');
+        }, 500);
+
         iconPopup.classList.remove('active');
         menuPopup.classList.remove('active');
         startY = null;
@@ -66,4 +109,3 @@ for (let i = 1; i <= 9; i++) {
     });
   }
 }
-});
