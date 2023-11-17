0b6d9667c972f004355e1d1dd132547935f07a71
Olexandr597 <Olexandr597@gmail.com>
2023-11-17 18:34:40 +0200

    conect coffee_popup

--- js/pop_up_section_menu.js
+++ js/pop_up_section_menu.js
@@ -1,32 +1,68 @@
-const iconPopup = document.querySelector('.burger_first');
-if (iconPopup) {
-  const menuPopup = document.querySelector('.pop_up_menu');
-  const iconCrossPopup = document.querySelector('.nav_line');
-  let startY;
+// const iconPopup = document.querySelector('.burger_first');
+// if (iconPopup) {
+//   const menuPopup = document.querySelector('.pop_up_menu');
+//   const iconCrossPopup = document.querySelector('.nav_line');
+//   let startY;
 
-  // Відкривання попапа при кліці
-  iconPopup.addEventListener("click", function (e) {
-    document.body.classList.toggle('_lock');
-    iconPopup.classList.toggle('active');
-    menuPopup.classList.toggle('active');
-  });
+//   // Відкривання попапа при кліці
+//   iconPopup.addEventListener("click", function (e) {
+//     document.body.classList.toggle('_lock');
+//     iconPopup.classList.toggle('active');
+//     menuPopup.classList.toggle('active');
+//   });
 
-  // Закриття попапа при свайпі вниз
-  document.addEventListener('touchstart', function (event) {
-    startY = event.touches[0].clientY;
-  });
+//   // Закриття попапа при свайпі вниз
+//   document.addEventListener('touchstart', function (event) {
+//     startY = event.touches[0].clientY;
+//   });
 
-  document.addEventListener('touchmove', function (event) {
-    if (!startY) return;
+//   document.addEventListener('touchmove', function (event) {
+//     if (!startY) return;
 
-    const currentY = event.touches[0].clientY;
-    const deltaY = currentY - startY;
+//     const currentY = event.touches[0].clientY;
+//     const deltaY = currentY - startY;
 
-    if (deltaY > 50 && event.target.classList.contains('nav_line')) {
-      document.body.classList.remove('_lock');
-      iconPopup.classList.remove('active');
-      menuPopup.classList.remove('active');
-      startY = null;
-    }
-  });
+//     if (deltaY > 50 && event.target.classList.contains('nav_line')) {
+//       document.body.classList.remove('_lock');
+//       iconPopup.classList.remove('active');
+//       menuPopup.classList.remove('active');
+//       startY = null;
+//     }
+//   });
+// }
+
+for (let i = 1; i <= 9; i++) {
+  const iconPopup = document.querySelector(`.coffee_${i}`);
+  const menuPopup = document.querySelector(`.coffee_popup_${i}`);
+
+  if (iconPopup && menuPopup) {
+    const iconCrossPopup = document.querySelector('.nav_line');
+    let startY;
+
+    // Відкривання попапа при кліці
+    iconPopup.addEventListener("click", function (e) {
+      document.body.classList.toggle('_lock');
+      iconPopup.classList.toggle('active');
+      menuPopup.classList.toggle('active');
+    });
+
+    // Закриття попапа при свайпі вниз
+    document.addEventListener('touchstart', function (event) {
+      startY = event.touches[0].clientY;
+    });
+
+    document.addEventListener('touchmove', function (event) {
+      if (!startY) return;
+
+      const currentY = event.touches[0].clientY;
+      const deltaY = currentY - startY;
+
+      if (deltaY > 50 && event.target.classList.contains('nav_line')) {
+        document.body.classList.remove('_lock');
+        iconPopup.classList.remove('active');
+        menuPopup.classList.remove('active');
+        startY = null;
+      }
+    });
+  }
 }
